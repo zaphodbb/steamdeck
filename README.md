ce62f88121b2e09fedc5b6639072dc9075e68a9e
Sean Mullen <mullens101@gmail.com>
2022-10-05 20:20:02 -0400

    Initial script

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # steamdeck
+Scripts I've written to deal with my personal steamdeck.
+
+openiv.sh - Use proton to install OpenIV mod system into the GTA V proton compatibility folder.
+
---
+++ openiv.sh
@@ -0,0 +1,6 @@
+#Add to GTA V Install
+export STEAM_COMPAT_CLIENT_INSTALL_PATH=/home/deck/.local/share/Steam
+export STEAM_COMPAT_DATA_PATH=/home/deck/.local/share/Steam/steamapps/compatdata/271590/pfx
+export WINEPREFIX=/home/deck/.local/share/Steam/steamapps/compatdata/271590/pfx
+~/.steam/root/steamapps/common/Proton\ 7.0/proton run ~/Downloads/ovisetup.exe
+
